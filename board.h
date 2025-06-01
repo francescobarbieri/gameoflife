4c64109bd84b1e61f02ad655013f74f9f4436169
Francesco Barbieri <fbarbieri.business@gmail.com>
2025-06-01 21:01:02 +0200

    printing square board

--- board.h
+++ board.h
@@ -4,12 +4,15 @@
 
 #include <vector>
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 class board {
   private:
     std::vector<std::vector<bool> > array;
   public:
     board(int size);
+    void set_cell(int x, int y, bool value);
     void print_board();
 };
 
@@ -17,14 +20,26 @@ board::board(int size) : array(50, std::vector<bool>(size, false))
 {
 }
 
+void board::set_cell(int x, int y, bool value)
+{
+  if(x < this->array.size() && y < this->array.size())
+    this->array[x][y] = value;
+}
+
 void board::print_board()
 {
+  std::cout << "\033[2J\033[1;1H";
   for(int i = 0; i < this->array.size(); i++) {
     for(int j = 0; j < this->array.size(); j++) {
-      std::cout << this->array[i][j];
+      if(this->array[i][j]) {
+        std::cout << "█ ";
+      } else {
+        std::cout << ". ";
+      }
     }
     std::cout << std::endl;
   }
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,6 +5,18 @@ using namespace std;
 int main () {
   board current(50);
   current.print_board();
+  current.set_cell(49, 49, true);
+  current.print_board();
+  current.set_cell(49, 49, false);
+  current.print_board();
+  current.set_cell(49, 49, true);
+  current.print_board();
+  current.set_cell(49, 49, false);
+  current.print_board();
+  current.set_cell(49, 49, true);
+  current.print_board();
+  current.set_cell(49, 49, false);
+  current.print_board();
 
   return 0;
 }
